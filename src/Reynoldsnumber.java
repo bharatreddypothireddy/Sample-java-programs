c3c34eb366c637b72eb324a69fa2d0cf3e805426
bharatreddypothireddy <bharatreddy.pothireddy@gmail.com>
2018-01-24 16:58:45 +0530

    Datediffformat

---
+++ src/Reynoldsnumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Reynoldsnumber {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter diameter :");
+        float D = x.nextFloat();
+        System.out.println("Enter velocity :");
+        float V = x.nextFloat();
+        System.out.println("Enter viscosity");
+        float mu = x.nextFloat();
+        System.out.println("Enter density :");
+        float rho = x.nextFloat();
+        float R = (D*V*rho)/mu;
+        System.out.println("Reynolds number is : "+R);
+    }
+}
