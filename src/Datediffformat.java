8ddab72d0f47b065ace372a8639222ec44a89d81
bharatreddypothireddy <bharatreddy.pothireddy@gmail.com>
2018-01-24 16:40:59 +0530

    Datediffformat

---
+++ src/Datediffformat.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+public class Datediffformat {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println(DateTimeFormatter.ofPattern("dd/MM/yyyy").format(today));
+    }
+}
